04b2b7943bbe6ef175163e36c6d3aab45e2c64b7
Dayana <dayanamdr20@gmail.com>
2023-04-22 23:30:28 +0300

    updates

--- BE/api/api/settings.py
+++ BE/api/api/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,12 +91,23 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sdi-project-db',
-        'USER': 'sdi-project',
-        'PASSWORD': '1120'
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'sdi-project-db',
+#         'USER': 'sdi-project',
+#         'PASSWORD': '1120'
+#     }
+# }
 
 # DATABASES = {
 #     'default': {

97cfeef30abfd1fa67631765a8cb87296161e8b7
Dayana <dayanamdr20@gmail.com>
2023-04-25 17:00:04 +0300

    Update database to psql

--- BE/api/api/settings.py
+++ BE/api/api/settings.py
@@ -100,21 +100,21 @@ WSGI_APPLICATION = 'api.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'sdi-project-db',
-#         'USER': 'sdi-project',
-#         'PASSWORD': '1120'
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_music',
+        'USER': 'postgres',
+        'PASSWORD': 'admin'
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

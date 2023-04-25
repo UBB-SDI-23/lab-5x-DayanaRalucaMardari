fb4dd5b248c9e52eabba57060d141b11a4266367
Dayana <dayanamdr20@gmail.com>
2023-04-25 17:52:38 +0300

    db sqlite

--- BE/api/api/settings.py
+++ BE/api/api/settings.py
@@ -100,23 +100,23 @@ WSGI_APPLICATION = 'api.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_music',
-        'USER': 'db_user',
-        'PASSWORD': 'db_pass',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'db_music',
+#         'USER': 'db_user',
+#         'PASSWORD': 'db_pass',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

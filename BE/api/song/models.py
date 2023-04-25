b80a039e2ee2784b3b142b3bc4828e65422b8c85
Dayana <dayanamdr20@gmail.com>
2023-04-25 22:48:06 +0300

    Add description field to Song

--- BE/api/song/models.py
+++ BE/api/song/models.py
@@ -8,6 +8,7 @@ class Song(models.Model):
     record_date = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
     label = models.CharField(max_length=100)
     length = models.CharField(max_length=20)
+    description = models.CharField(max_length=300, blank=True, null=True)
     album_id = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='children')
 
     def __str__(self):
